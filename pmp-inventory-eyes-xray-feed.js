a6acd6eceec4f21e02e13b5c4db851b363d00b51
pmpbird <pmpbird@gmail.com>
2026-05-10 12:49:28 -0700

    Add feed rule note

--- pmp-inventory-eyes-xray-feed.js
+++ pmp-inventory-eyes-xray-feed.js
@@ -1,2 +1,3 @@
-// PMP Inventory Eyes X-Ray feed placeholder
+// PMP Inventory Eyes X-Ray feed
+// Read-only rule: X-Ray may read Inventory Eyes summaries, but must not modify the working Lossless Quality keys.
 window.PMP_INVENTORY_EYES_XRAY_FEED_READY = true;
