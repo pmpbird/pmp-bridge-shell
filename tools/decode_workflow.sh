0f2c8e71f2d09e2d7d7eab9075509042f9c4cfeb
pmpbird <pmpbird@gmail.com>
2026-06-15 21:10:07 -0700

    Add workflow cleanup helper

---
+++ tools/decode_workflow.sh
@@ -0,0 +1,2 @@
+base64 -d tools/workflow.yml.b64 > .github/workflows/packet_015_pass_002_current_runtime_family.yml
+rm tools/workflow.yml.b64 tools/decode_workflow.sh .github/workflows/restore_runtime_family.yml tools/restore_runtime_family_workflow.sh
