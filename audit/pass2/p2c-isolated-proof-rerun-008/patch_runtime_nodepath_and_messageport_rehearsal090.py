1a82154463e2fdcf0281ca8c9cda20b0e22a944b
pmpbird <pmpbird@gmail.com>
2026-07-20 00:43:07 -0700

    Repair 090 A-002 single-page historic matrix

--- audit/pass2/p2c-isolated-proof-rerun-008/patch_runtime_nodepath_and_messageport_rehearsal090.py
+++ audit/pass2/p2c-isolated-proof-rerun-008/patch_runtime_nodepath_and_messageport_rehearsal090.py
@@ -29,6 +29,7 @@ a002_historic_context_isolation = here / 'patch_a002_historic_context_isolation_
 ci_lane_closure = here / 'patch_ci_lane_closure_rehearsal109.py'
 a002_historic_lane_lifecycle = here / 'patch_a002_historic_lane_lifecycle_rehearsal110.py'
 ci_lane_lifecycle = here / 'patch_ci_lane_lifecycle_rehearsal111.py'
+a002_single_page_historic_matrix = here / 'patch_a002_single_page_historic_matrix_rehearsal112.py'
 bundle_root = pathlib.Path(a.bundle_root)
 prepare_target = bundle_root / 'prepare_disposable_proof_002.py'
 policy_target = bundle_root / 'policy-template.json'
@@ -162,4 +163,11 @@ subprocess.run([
     '--evidence-dir', a.evidence_dir,
 ], check=True)
 
-print('REHEARSAL111_CI_LANE_LIFECYCLE_AND_PRIOR_REPAIRS_APPLIED')
+subprocess.run([
+    sys.executable,
+    str(a002_single_page_historic_matrix),
+    '--path', str(target),
+    '--evidence-dir', a.evidence_dir,
+], check=True)
+
+print('REHEARSAL112_A002_SINGLE_PAGE_HISTORIC_MATRIX_AND_PRIOR_REPAIRS_APPLIED')
